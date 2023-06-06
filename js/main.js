c3a4f181e3c677978e5847c86d743e78aa5c96de
Logis <you@example.commuruganmsd7710@gmail.com>
2023-06-06 16:49:33 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+let numbers=[1,2,3,4,5,6,7,8];
+let fact=1;
+numbers.forEach(function(number)
+			{
+				fact=fact*number;
+				
+			document.write("factorial of"+ number+"="+" "+ fact, "<br>");
+				
+			})		
+
+
+
+
